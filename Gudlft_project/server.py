ea739832e7b293878c5b4ada3ee6687e2e73d287
Shinkhan <hzabdelwahid@gmail.com>
2024-02-12 13:45:20 +0100

    Reverting changes made in server.py file, those changes are not supposed to be in this branch

--- Gudlft_project/server.py
+++ Gudlft_project/server.py
@@ -81,21 +81,18 @@ def showSummary():
 
     return render_template('welcome.html',club=club,competitions=competitions)
 
-@app.route('/book/<competition_name>/<club_name>')
-def book(competition_name, club_name):
-    competitions = loadCompetitions()
+@app.route('/book/<competition>/<club>')
+def book(competition,club):
+
     clubs = loadClubs()
-    try:
-        foundClub = find_item_by_attribute(clubs, 'name', club_name)
-        #foundClub = [c for c in clubs if c['name'] == club][0]
-        
-        foundCompetition = find_item_by_attribute(competitions, 'name', competition_name)
-        #foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    except KeyError:
-        flash("Invalid form data. Please try again.")
-        return redirect(url_for('index'))
+    competitions = loadCompetitions()
 
+    foundClub = find_item_by_attribute(clubs, 'name', club)
+    #foundClub = [c for c in clubs if c['name'] == club][0]
 
+    foundCompetition = find_item_by_attribute(competitions, 'name', competition)
+    #foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    
     if foundClub and foundCompetition:
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
