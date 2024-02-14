b5fb9b7ae2dff1387770e61cf8dafa010704a951
Shinkhan <hzabdelwahid@gmail.com>
2024-02-14 18:35:12 +0100

    Fixing issues in imports for testing. Fixing issues in server.py in book route.

--- Gudlft_project/tests/unit/unit_tests.py
+++ Gudlft_project/tests/unit/unit_tests.py
@@ -1,5 +1,10 @@
+import sys
+import os
 import pytest
-from ...server import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+from Gudlft_project.server import app
 
 ## 1. ERROR: Error handling if competition or club not found in book route
 def test_book(client):
