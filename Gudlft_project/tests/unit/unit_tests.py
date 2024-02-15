5187d990c4719ff7d3225a252787709090f8e11f
Shinkhan <hzabdelwahid@gmail.com>
2024-02-15 12:55:46 +0100

    Modifying a comment

--- Gudlft_project/tests/unit/unit_tests.py
+++ Gudlft_project/tests/unit/unit_tests.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 from Gudlft_project.server import app
 
-## 1. ERROR: Error handling if competition or club not found in book route
+## 2. ERROR: Error handling if competition or club not found in book route
 def test_book(client):
     response = client.get('/book/unknown_competition/unknown_club', follow_redirects=True)
     assert response.status_code == 200
